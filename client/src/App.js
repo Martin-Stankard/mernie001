61bd0c00e69d23760a5e1c8e3fb8bc66934f58cd
Martin <martin.stankard@hotmail.com>
2018-06-29 06:49:59 -0400

    buttons look good

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import AppNavbar from './components/app.navbar';
+import AppNavbar from './components/appnavbar';
+import ShoppingList from './components/shoppinglist';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppNavbar/>
-        <h1>Howdy</h1>
+        <ShoppingList/>
       </div>
     );
   }
