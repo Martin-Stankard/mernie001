bcc2272db135c8d509f7d4fc98877ae39560e672
Martin <martin.stankard@hotmail.com>
2018-06-30 19:56:27 -0400

    mid 7

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,13 +1,9 @@
 import uuid from 'uuid';
-import {GET_ITEMS,ADD_ITEM,DELETE_ITEM} from '../actions/types';
+import {GET_ITEMS,ADD_ITEM,DELETE_ITEM, ITEMS_LOADING} from '../actions/types';
 
 const initialState = {
-    items: [
-        {id:uuid(), name: 'Eggs'},
-        {id:uuid(), name: 'Milk'},
-        {id:uuid(), name: 'Steak'},
-        {id:uuid(), name: 'Candy'}
-    ]
+    items: [],
+    loading:false
 };
 
 
@@ -15,7 +11,9 @@ export default function(state = initialState , action){
     switch(action.type){
         case GET_ITEMS:
             return{
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
             };
         
         case DELETE_ITEM:
@@ -29,6 +27,12 @@ export default function(state = initialState , action){
                 ...state,
                 items: [action.payload, ...state.items]
             } 
+        
+        case ITEMS_LOADING:
+            return{
+                ...state,
+                loading:true 
+            }
 
         default:
             return state;
