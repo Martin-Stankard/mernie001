7f9002fcde96ae574264bfd9fa87be40479d2da6
Martin <martin.stankard@hotmail.com>
2018-06-30 10:19:22 -0400

    delete is working locally

--- client/src/components/shoppinglist.js
+++ client/src/components/shoppinglist.js
@@ -3,7 +3,7 @@ import {Container, ListGroup, ListGroupItem, Button} from 'reactstrap';
 import {CSSTransition, TransitionGroup } from 'react-transition-group';
 import uuid from 'uuid';
 import {connect} from 'react-redux';
-import {getItems} from '../actions/itemActions';
+import {getItems, deleteItem} from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
@@ -12,6 +12,10 @@ class ShoppingList extends Component {
         this.props.getItems();
     }
     
+    onDeleteClick= (id)=>{
+        this.props.deleteItem(id);
+    };
+
     render(){
         
         const { items } = this.props.item;
@@ -39,11 +43,7 @@ class ShoppingList extends Component {
                                         className="remove-btn"
                                         color = "danger"
                                         size = "sm"
-                                        onClick = { () => {
-                                            this.setState(state => ({
-                                                items:state.items.filter(item => item.id !== id)
-                                            }));                                                
-                                        }}
+                                        onClick = {this.onDeleteClick.bind(this,id) }
                                         >&times;</Button>
                                         {name}
                                 </ListGroupItem>
@@ -66,5 +66,5 @@ const mapStateToProps = state =>({
 });
 
 export default connect(mapStateToProps,{
-    getItems
+    getItems,deleteItem
 })(ShoppingList);
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -18,6 +18,11 @@ export default function(state = initialState , action){
                 ...state
             };
         
+        case DELETE_ITEM:
+            return{
+                ...state,
+                items:state.items.filter(item => item.id !== action.payload)
+            }
         
         default:
             return state;
